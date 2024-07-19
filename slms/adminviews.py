436a495086f834e9d4b0692d4c04d1941fc0bc7b
Sargun Singh <sargun.singh@beehyv.com>
2024-07-19 10:46:12 +0530

    Hi chnages

--- slms/adminviews.py
+++ slms/adminviews.py
@@ -1,11 +1,6 @@
 
 from django.shortcuts import HttpResponse
 from slmsapp.models import CustomUser, Staff
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
